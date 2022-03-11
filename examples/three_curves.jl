467420edc798708209eb6b024682d0383f725641
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2022-03-11 15:46:19 +0100

    Update three_curves.jl

--- examples/three_curves.jl
+++ examples/three_curves.jl
@@ -1,17 +1,28 @@
+using GeometricClusterAnalysis
+using LinearAlgebra
+using Plots
 using Random
 using RCall
-using Plots
-using GeometricClusterAnalysis
+using Test
 
-nsignal = 500 # number of signal points
-nnoise = 200 # number of outliers
-dim = 2 # dimension of the data
-sigma = 0.02 # standard deviation for the additive noise
+R"""
+library("here")
+library("ggplot2")
+library("ggforce")
+"""
+
+nrow(A::AbstractMatrix) = size(A)[1]
+ncol(A::AbstractMatrix) = size(A)[2]
+
+nsignal = 100   # number of signal points
+nnoise = 20     # number of outliers
+dim = 2         # dimension of the data
+sigma = 0.02    # standard deviation for the additive noise
 nb_clusters = 3 # number of clusters
-k = 10 # number of nearest neighbors
-c = 50 # number of ellipsoids
-iter_max = 100 # maximum number of iterations of the algorithm kPLM
-nstart = 10 # number of initializations of the algorithm kPLM
+k = 10           # number of nearest neighbors
+c = 5          # number of ellipsoids
+iter_max = 100  # maximum number of iterations of the algorithm kPLM
+nstart = 10     # number of initializations of the algorithm kPLM
 
 @rput nsignal
 @rput nnoise
@@ -25,12 +36,19 @@ nstart = 10 # number of initializations of the algorithm kPLM
 
 rng = MersenneTwister(1234)
 
-data = noisy_three_curves( rng, nsignal, nnoise, sigma, dim)
-
+data = noisy_three_curves(rng, nsignal, nnoise, sigma, dim)
 
 function f_Σ!(Σ) end
 
+@rput nsignal
+
 dist_func = kplm(rng, data.points, k, c, nsignal, iter_max, nstart, f_Σ!)
+
+P = collect(data.points')
+
+@show size(P)
+@rput P
+
 
 centers = vcat(dist_func.centers'...)
 @rput centers
@@ -43,11 +61,10 @@ Sigma = dist_func.Σ
 
 
 R"""
-library("ggplot2")
-library("ggforce")
 
 dic_lambda <- function(x,y,eigval,c,omega){
-  f_moy = sum((eigval-((x+y)/2)^2)/(eigval+((x+y)/2))^2*eigval*c^2)
+  h <- (x+y)/2
+  f_moy = sum((eigval-h^2) / (eigval+h)^2 * eigval * c^2)
   err = abs(f_moy - omega)
   if(f_moy>omega){
     x = (x+y)/2
@@ -58,7 +75,22 @@ dic_lambda <- function(x,y,eigval,c,omega){
     return(list(x = x, y = y, err = err))
   }
 }
+"""
 
+function dic_lambda(x, y, eigval, c, omega)
+    h = (x + y) / 2
+    f_moy = sum((eigval .- h^2) ./ (eigval .+ h).^2 .* eigval .* c.^2)
+    err = abs(f_moy - omega)
+    if f_moy > omega
+        x = (x + y) / 2
+    else
+        y = (x + y) / 2
+    end
+    return x, y, err
+end
+
+
+R"""
 lambda_solution <- function(omega,eigval,c){
   res = list(x = 0, y = 2*max(sqrt(eigval)), err = Inf)
   while(res$err>=0.001){
@@ -68,7 +100,24 @@ lambda_solution <- function(omega,eigval,c){
   }
   return((x+y)/2)
 }
+"""
 
+function lambda_solution(omega, eigval, c)
+
+    res = 0, 2 * maximum(sqrt.(eigval)), Inf
+    x = res[1]
+    y = res[2]
+
+    while res[3] >= 0.001
+        x = res[1]
+        y = res[2]
+        res = dic_lambda(x, y, eigval, c, omega)
+    end
+    return (x + y) / 2
+end
+
+
+R"""
 r_solution <- function(omega_1,omega_2,eigval,c){ # C'est le r^2 si les omega sont positifs...
   if(sum(c^2)<=omega_2-omega_1){
     return (omega_2)
@@ -78,7 +127,19 @@ r_solution <- function(omega_1,omega_2,eigval,c){ # C'est le r^2 si les omega so
     return(omega_2+sum(((lambda*c)/(lambda+eigval))^2*eigval))
   }
 }
+"""
 
+function r_solution(ω₁, ω₂, eigval, c) # C'est le r^2 si les omega sont positifs...
+    if sum(c .^ 2) <= ω₂ - ω₁
+        return ω₂
+    else
+        λ = lambda_solution(ω₂ - ω₁, eigval, c)
+        return ω₂ .+ sum(((λ .* c) ./ (λ .+ eigval)).^2 .* eigval)
+    end
+end
+
+
+R"""
 intersection_radius <- function(Sigma_1,Sigma_2,c_1,c_2,omega_1,omega_2){
   if(!(all.equal(t(Sigma_1),Sigma_1)==TRUE) || !(all.equal(t(Sigma_2),Sigma_2)==TRUE)){
     return("Sigma_1 and Sigma_2 should be symmetrical matrices")
@@ -115,6 +176,67 @@ intersection_radius <- function(Sigma_1,Sigma_2,c_1,c_2,omega_1,omega_2){
   return(r_sq)
 }
 
+
+"""
+
+function swap!(a, b)
+    for i in eachindex(a, b)
+        @inbounds a[i], b[i] = b[i], a[i]
+    end
+end
+
+function intersection_radius(Σ₁, Σ₂, μ₁, μ₂, ω₁, ω₂)
+
+    @assert issymmetric(Σ₁)
+    @assert issymmetric(Σ₂)
+    @assert length(μ₁) == length(μ₂)
+    @assert length(μ₁) == nrow(Σ₁)
+    @assert length(μ₂) == nrow(Σ₂)
+
+    if ω₁ > ω₂
+        ω₁, ω₂ = ω₂, ω₁
+        swap!(Σ₁, Σ₂)
+        swap!(μ₁, μ₂)
+    end
+
+    eig_1 = eigen(Σ₁)
+    P_1 = eig_1.vectors
+    sq_D_1 = Diagonal(sqrt.(eig_1.values))
+    inv_sq_D_1 = Diagonal(sqrt.(eig_1.values) .^ (-1))
+
+    eig_2 = eigen(Σ₂)
+    P_2 = eig_2.vectors
+    inv_D_2 = Diagonal(eig_2.values .^ (-1))
+
+    tilde_Sigma = sq_D_1 * P_1'P_2 * inv_D_2 * P_2'P_1 * sq_D_1
+
+    tilde_eig = eigen(tilde_Sigma)
+    tilde_eigval = reverse(tilde_eig.values)
+    tilde_P = tilde_eig.vectors
+    tilde_c = reverse(tilde_P' * inv_sq_D_1 * P_1' * (μ₂ - μ₁))
+
+    return r_solution(ω₁, ω₂, tilde_eigval, tilde_c)
+
+end
+
+R"""
+i <- 1
+j <- 2
+
+print(intersection_radius(Sigma[[i]],Sigma[[j]], means[i,],means[j,], weights[i],weights[j]))
+
+"""
+
+
+i, j = 1, 2
+intersection_radius(dist_func.Σ[i],dist_func.Σ[j],
+                    dist_func.μ[i],dist_func.μ[j],
+                    dist_func.weights[i],weights[j])
+
+#=
+
+
+R"""
 # Distance matrix for the graph filtration
 
 build_matrice_hauteur <- function(means,weights,cov_matrices,indexed_by_r2 = TRUE){
@@ -149,13 +271,66 @@ build_matrice_hauteur <- function(means,weights,cov_matrices,indexed_by_r2 = TRU
     return(sqrt(matrice_hauteur))
   }
 }
+"""
 
+
+"""
+    build_matrix(result; indexed_by_r2 = true)
+
+Distance matrix for the graph filtration
+
+- indexed_by_r2 = true always work 
+- indexed_by_r2 = false requires elements of weigths to be non-negative.
+- indexed_by_r2 = false for the sub-level set of the square-root of non-negative power functions : the k-PDTM or the k-PLM (when determinant of matrices are forced to be 1)
+"""
+function build_matrix(result; indexed_by_r2 = true)
+
+    c = length(result.μ)
+
+    @assert c == length(result.weights)
+
+    mh = zeros(c, c)
+
+    if c == 1
+        if indexed_by_r2
+            return [first(weights)]
+        else # Indexed by r -- only for non-negative functions (k-PDTM and k-PLM with det = 1)
+            return [sqrt(first(weights))]
+        end
+    end
+
+    for i = 1:c
+        mh[i, i] = weights[i]
+    end
+
+    for i = 2:c, j = 1:(i-1)
+        mh[i, j] = intersection_radius(
+            result.Σ[i],
+            result.Σ[j],
+            result.μ[i],
+            means[j,],
+            weights[i],
+            weights[j],
+        )
+    end
+
+    if indexed_by_r2
+        return mh
+    else
+        return sqrt.(mh)
+    end
+end
+
+#mh = build_matrix(dist_func)
+
+
+R"""
 matrice_hauteur = build_matrice_hauteur(means,weights,Sigma,indexed_by_r2 = TRUE)
 """
 
 @rget matrice_hauteur
 
-#=
+#@test matrice_hauteur ≈ mh
 
 path = "."
 
@@ -220,7 +395,7 @@ hierarchical_clustering_lem <- function(matrice_hauteur,Stop = Inf,Seuil = Inf,s
   # For this, we look at the persistence diagram of the components : (x-axis Birth ; y-axis Death)
   # store_all_colors = TRUE : in the list Couleurs, we store all configurations of colors, for every step.
   # Thresholding :
-  
+
   # Matrice_hauteur is modified such that diagonal elements are non-decreasing
   mh_sort = sort(diag(matrice_hauteur),index.return = TRUE)
   c = sum(mh_sort$x<=Seuil)
@@ -228,11 +403,11 @@ hierarchical_clustering_lem <- function(matrice_hauteur,Stop = Inf,Seuil = Inf,s
   if(c == 0){
     return(list(color = c(),Naissance = c(),Mort = c(),Indices_depart = c()))
   }
-  
+
   if(c == 1){
     return(list(color = c(1),Naissance = c(mh_sort$x[1]),Mort = c(Inf),Indices_depart = c(mh_sort$ix[1])))
   }
-  
+
   Indices_depart = mh_sort$ix[1:c] # Initial indices of the centers born at time mh_sort$x
   Naissance = mh_sort$x[1:c]
   Mort = rep(Inf,c) # Arbitrary 
@@ -244,7 +419,7 @@ hierarchical_clustering_lem <- function(matrice_hauteur,Stop = Inf,Seuil = Inf,s
   }
   step = 1
   matrice_dist = matrix(data = Inf,nrow = c,ncol = c) # The new matrice_hauteur
-  
+
   for(i in 1:c){
     matrice_dist[i,i] = Naissance[i]
   }
@@ -253,12 +428,12 @@ hierarchical_clustering_lem <- function(matrice_hauteur,Stop = Inf,Seuil = Inf,s
       matrice_dist[i,j] = min(matrice_hauteur[Indices_depart[i],Indices_depart[j]],matrice_hauteur[Indices_depart[j],Indices_depart[i]])
     } # i>j : component i appears after component j, they dont merge before i appears
   }
-  
+
   # Initialization :
-  
+
   continu = TRUE
   indice = 1 # Only components with index not larger than indice are considered
-  
+
   indice_hauteur = which.min(matrice_dist[1:indice,])
   ihj = (indice_hauteur-1) %/% c + 1
   ihi = indice_hauteur - (ihj-1) * c
@@ -267,7 +442,7 @@ hierarchical_clustering_lem <- function(matrice_hauteur,Stop = Inf,Seuil = Inf,s
     Temps_step = list(temps_step)
   }
   # ihi >= ihj since the matrix is triangular inferior with infinity value above the diagonal
-  
+
   while(continu){
     if(temps_step == matrice_dist[ihi,ihi]){# Component ihi birth
       couleurs[ihi] = ihi
@@ -565,120 +740,4 @@ ggsave(filename = filename,path=path)
 
 """
 
-
-function dic_lambda(x,y,eigval,c,omega)
-    f_moy = sum((eigval-((x+y)/2)^2)/(eigval+((x+y)/2))^2*eigval*c^2)
-    err = abs(f_moy - omega)
-    if f_moy>omega
-        x = (x+y)/2
-    else
-        y = (x+y)/2
-    end
-    return x, y, err
-end
-
-function lambda_solution(omega,eigval,c)
-
-    x, y, err = list(x = 0, y = 2*max(sqrt(eigval)), err = Inf)
-
-    while err >= 0.001
-        _, _, err = dic_lambda(x,y,eigval,c,omega)
-    end
-    return (x+y)/2
-end
-
-function r_solution(omega_1,omega_2,eigval,c) # C'est le r^2 si les omega sont positifs...
-    if sum(c^2)<=omega_2-omega_1
-        return omega_2
-    else
-        lambda = lambda_solution(omega_2-omega_1,eigval,c)
-        return omega_2+sum(((lambda*c)/(lambda+eigval))^2*eigval)
-    end
-end
-
-nrow(A::AbstractMatrix) = size(A)[1]
-ncol(A::AbstractMatrix) = size(A)[2]
-
-
-function intersection_radius(Sigma_1,Sigma_2,c_1,c_2,omega_1,omega_2)
-
-  @assert issymmetric(Sigma_1) 
-  @assert issymmetric(Sigma_2)
-  @assert length(c_1) == length(c_2)
-  @assert length(c_1) == nrow(Sigma_1)
-  @assert length(c_2) == nrow(Sigma_2)
-  
-  if(nrow(Sigma_1)!=length(c_1) || nrow(Sigma_2)!=length(c_2) || length(c_1)!=length(c_2)){
-    return("c_1 and c_2 should have the same length, this length should be the number of row of Sigma_1 and of Sigma_2")
-  }
-  c_1 = matrix(c_1,nrow = length(c_1),ncol = 1)
-  c_2 = matrix(c_2,nrow = length(c_2),ncol = 1)
-  if(omega_1>omega_2){
-    omega_aux = omega_1
-    omega_1 = omega_2
-    omega_2 = omega_aux
-    Sigma_aux = Sigma_1
-    Sigma_1 = Sigma_2
-    Sigma_2 = Sigma_aux
-    c_aux = c_1
-    c_1 = c_2
-    c_2 = c_aux # Now, omega_1\leq omega_2
-  }
-  eig_1 = eigen(Sigma_1)
-  P_1 = eig_1$vectors
-  sq_D_1 = diag(sqrt(eig_1$values))
-  inv_sq_D_1 = diag(sqrt(eig_1$values)^(-1))
-  eig_2 = eigen(Sigma_2)
-  P_2 = eig_2$vectors
-  inv_D_2 = diag(eig_2$values^(-1))
-  tilde_Sigma = sq_D_1%*%t(P_1)%*%P_2%*%inv_D_2%*%t(P_2)%*%P_1%*%sq_D_1
-  tilde_eig = eigen(tilde_Sigma)
-  tilde_eigval = tilde_eig$values
-  tilde_P = tilde_eig$vectors
-  tilde_c = t(tilde_P)%*%inv_sq_D_1%*%t(P_1)%*%(c_2-c_1)
-  r_sq = r_solution(omega_1,omega_2,tilde_eigval,tilde_c)
-  return(r_sq)
-end
-
-
-# Distance matrix for the graph filtration
-
-"""
-    build_matrix(result; indexed_by_r2 = true)
-
-indexed_by_r2 = true always work ; indexed_by_r2 = false requires elements of weigts to be non-negative.
-indexed_by_r2 = FALSE for the sub-level set of the square-root of non-negative power functions : the k-PDTM or the k-PLM (when determinant of matrices are forced to be 1)
-"""
-function build_matrix(result; indexed_by_r2 = true)
-
-  c = length(result.μ)
-
-  @assert c == length(result.weights)
-
-  mh = zeros(c, c)
-
-  if c==1
-      if indexed_by_r2
-	      return [first(weights)]
-      else # Indexed by r -- only for non-negative functions (k-PDTM and k-PLM with det = 1)
-          return [sqrt(first(weights))]
-	  end
-  end
-
-  for i in 1:c
-      mh[i,i] = weights[i]
-  end
-
-  for i in 2:c, j in 1:(i-1)
-      mh[i,j] = intersection_radius(result.Σ[i],result.Σ[j],result.μ[i],means[j,],weights[i],weights[j])
-  end
-
-  if indexed_by_r2 
-      return mh
-  else
-      return sqrt.(mh) 
-  end
-end
- =#
-
-scatter(data, c=cgrad(:matter, 5, categorical = true))
+=#

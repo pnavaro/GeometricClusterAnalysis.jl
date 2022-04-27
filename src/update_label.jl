59c0328d94f2ab67b3c2c8140f1321d812334dd8
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2022-04-27 08:58:42 +0200

    Update update_label.jl

--- src/update_label.jl
+++ src/update_label.jl
@@ -2,29 +2,31 @@
     update_label!(label)
 
 - label is a vector of integers with possibly value 0
-if there are m different positive integers, set f, a bijective map between these integers and 1:m ; and f(0)=0
+
+if there are m different positive integers, set f, a bijective map between 
+these integers and 1:m ; and f(0)=0
 return f(label).
 """
 function update_label!(label)
 
-    sort_lab = sort(label, index.return = TRUE)
+    sort_lab = sortperm(label)
 
-    new_sort_label = rep(0,length(label))
+    new_sort_label = zero(label)
     new_lab = 1
-    lab = sort_lab$x[1]
+    lab = label[sort_lab[1]]
     new_sort_label[1] = new_lab
-    for(i in 2:length(label)){
-      if(sort_lab$x[i]!=lab)
-        new_lab = new_lab + 1
-        lab = sort_lab$x[i]
-      end
-      new_sort_label[i] = new_lab
+    for i = 2:length(label)
+        if label[sort_lab[i]] != lab
+            new_lab = new_lab + 1
+            lab = label[sort_lab[i]]
+        end
+        new_sort_label[i] = new_lab
     end
-    new_label = rep(0,length(label))
-    for(i in 1:length(label))
-      new_label[sort_lab$ix[i]] = new_sort_label[i]
+    new_label = zero(label)
+    for i in eachindex(label)
+        new_label[sort_lab[i]] = new_sort_label[i]
     end
-    return(new_label)
+
+    return new_label
 
 end
-

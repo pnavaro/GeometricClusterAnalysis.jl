471f43a3d78444bacf4ab523c41a39d1a29848e1
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2023-03-05 21:26:00 +0100

    Add R examples in fleas example

--- examples/dataset_fleas.jl
+++ examples/dataset_fleas.jl
@@ -163,6 +163,49 @@ p2 = plot_pointset(points, col_tomato)
 plot(p1, p2, layout = l)
 # -
 
+R"""
+source(here::here("R","plot_pointclouds_centers.R"))
+source(here::here("R","DTM_filtration.R"))
+source(here::here("R","functions_for_evaluating_methods.R"))
+
+dataset = tourr::flea
+P = dataset[,1:6]
+true_color = c(rep(1,21),rep(2,22),rep(3,31))
+P = scale(P)
+
+source(here::here("R", "tomato.R"))
+source(here::here("R", "hierarchical_clustering_complexes.R"))
+print("tomato")
+col_tomato = clustering_Tomato(3,P,10,100,nrow(P),1.9,100,10)$label
+print(aricode::NMI(col_tomato,true_color))
+"""
+
+
+# ## DBSCAN
+
+R"""
+col_dbscan = dbscan::dbscan(P, 1.5,minPts = 10)$cluster
+print(aricode::NMI(col_dbscan,true_color))
+"""
+
+clusters = dbscan(x, 1.5, min_neighbors = 5, min_cluster_size = 10)
+p = plot(aspect_ratio=true)
+for cluster in clusters
+    scatter!(p, x[1, cluster.core_indices], x[2, cluster.core_indices] )
+end
+p
+
+# +
+import ClusterAnalysis
+
+m = ClusterAnalysis.dbscan(points, 1.5, 10)
+p = plot(aspect_ratio = true)
+for cluster in m.clusters
+    scatter!(points[cluster,1], points[cluster, 4])
+end
+p
+# -
+
 # ## k-PLM 
 
 # +
@@ -194,10 +237,33 @@ p2 = plot_pointset(points, col_kplm)
 plot(p1, p2, layout = l)
 # -
 
-png("kplm")
+R"""
+source(here::here("R", "kplm.R"))
+source(here::here("R", "ellipsoids_intersection.R"))
+col_PLM = clustering_PLM(3,P,10,50,nrow(P),100,10,nb_means_removed = 0)$label
+print(aricode::NMI(col_PLM,true_color))
+"""
+
+R"""
+f_Sigma <- function(Sigma){return(Sigma)}
+col_PLM_nonhier = kplm(P,10,3,nrow(P),100,10,f_Sigma)$color
+print(aricode::NMI(col_PLM_nonhier,true_color))
+"""
 
 # ## Witnessed
 
+
+# +
+R"""
+source(here::here("R", "kpdtm.R"))
+col_witnessed = clustering_witnessed(3,P,10,50,nrow(P),100,10)$label
+print(aricode::NMI(col_witnessed,true_color))
+"""
+
+l = @layout [a b]
+p1 = plot_pointset(points, true_colors)
+p2 = plot_pointset(points, Int.(rcopy(R"col_witnessed")))
+plot(p1, p2, layout = l)
 
 # +
 μ, ω, colors = k_witnessed_distance(x, k, c, nsignal)
@@ -238,7 +304,16 @@ p2 = plot_pointset(points, kpdtm_colors)
 plot(p1, p2, layout = l)
 # -
 
-png("kpdtm")
+R"""
+source(here::here("R","kpdtm.R"))
+col_PDTM_nonhier = Trimmed_kPDTM(P,10,3,nrow(P),100,10)$color
+print(aricode::NMI(col_PDTM_nonhier,true_color))
+"""
+
+R"""
+col_PDTM = clustering_PDTM(3,P,10,50,nrow(P),100,10)$label
+print(aricode::NMI(col_PDTM,true_color))
+"""
 
 # ## Power function Buchet et al.
 #
@@ -265,6 +340,11 @@ p2 = plot_pointset(points, buchet_colors)
 plot(p1, p2, layout = l)
 # -
 
+R"""
+col_power = clustering_power_function(3,P,10,30,nrow(P),100,10)$label
+print(aricode::NMI(col_power,true_color))
+"""
+
 # ## DTM filtration
 #
 
@@ -290,6 +370,11 @@ p2 = plot_pointset(points, dtm_colors)
 plot(p1, p2, layout = l)
 # -
 
+R"""
+col_DTM_filtration = clustering_DTM_filtration(3,P,10,30,nrow(P),100,10)$label
+print(aricode::NMI(col_DTM_filtration,true_color))
+"""
+
 # ## Spectral with `specc`
 
 spectral_colors = rcopy(R"kernlab::specc(P, centers = 3)")
@@ -297,6 +382,23 @@ println("NMI = $(mutualinfo(true_colors, spectral_colors))")
 l = @layout [a b]
 p1 = plot_pointset(points, true_colors)
 p2 = plot_pointset(points, spectral_colors)
+plot(p1, p2, layout = l)
+
+l = @layout [a b]
+p1 = plot_pointset(points, true_colors)
+p2 = plot_pointset(points, Int.(rcopy(R"col_tomato")))
+plot(p1, p2, layout = l)
+
+R"""
+col_spectral = kernlab::specc(P, centers=3)
+col_spectral2 = rep(0,nrow(P))
+for(i in 1:nrow(P)){col_spectral2[i] = col_spectral[[i]]}
+print(aricode::NMI(col_spectral2,true_color))
+"""
+
+l = @layout [a b]
+p1 = plot_pointset(points, true_colors)
+p2 = plot_pointset(points, Int.(rcopy(R"col_PLM")))
 plot(p1, p2, layout = l)
 
 
